1e6d1eed3479fea3ed3a9026f74f2468829665bf
Guilherme Crespo <guilhermecresppo20@gmail.com>
2025-06-23 01:16:48 -0300

    Commit Inicial do projeto com implementações básicas

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "br.edu.ifsp.dmo2.healthsmartapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "br.edu.ifsp.dmo2.healthsmartapp"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation (platform("com.google.firebase:firebase-auth:22.3.1"))
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/br/edu/ifsp/dmo2/healthsmartapp/ui/SignUpActivity.kt
@@ -0,0 +1,19 @@
+package br.edu.ifsp.dmo2.healthsmartapp.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo2.healthsmartapp.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_sign_up)
+    }
+
+
+}
